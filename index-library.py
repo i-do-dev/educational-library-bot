c495d79d80489c9404b9fe75e2bc5eea39fa25fa
Waqar Muneer <i.do.dev@outlook.com>
2024-05-14 18:20:01 +0500

    getting resource files and preparing langchain documents with metadata

--- index-library.py
+++ index-library.py
@@ -1,18 +1,12 @@
-""" SELECT GROUP_CONCAT(c.title SEPARATOR '>') AS parentcollections
-FROM
-    resources r
-JOIN
-    collectionelements ce ON r.resourceid = ce.resourceid
-JOIN
-    resources c ON ce.collectionid = c.resourceid
-WHERE
-    r.type = 'resource'
-    AND r.resourceid = 11415;
- """
 import mysql.connector
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from langchain_community.document_loaders import PyPDFLoader
+import boto3
+import boto3.session
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from bs4 import BeautifulSoup
 
 # load the environment variables
 load_dotenv()
@@ -23,17 +17,34 @@ cnx = mysql.connector.connect(user=os.getenv('MYSQL_USER'), password=os.getenv('
                               database=os.getenv('MYSQL_DATABASE'))
 cursor = cnx.cursor()
 
-# query to get the resources
-resources_query = ("SELECT resourceid, title, pageurl, active, description, content, keywords FROM resources WHERE type = 'resource' AND active='T' limit 5")
-resources_df = pd.read_sql(resources_query, cnx)
+query_offset = 0
+query_limit = 1
+
+# query to get the resourcefiles
+resourcefiles_query = (f"""
+    SELECT 
+        rf.fileid, rf.resourceid, rf.filename, rf.ext, rf.s3path,
+        r.title, r.pageurl, r.active, r.description, r.content, r.keywords
+    FROM 
+        resourcefiles rf
+    LEFT JOIN 
+        resources r ON rf.resourceid = r.resourceid
+    WHERE 
+        r.type = 'resource' AND r.active = 'T' AND rf.ext = 'pdf' AND s3path IS NOT NULL AND s3path <> '' 
+    ORDER BY rf.fileid
+    LIMIT {query_limit} OFFSET {query_offset};
+""")
+
+# Read the query result into a pandas DataFrame
+resourcefiles_df = pd.read_sql(resourcefiles_query, cnx)
 
 # create empty lists to store the education levels, subject areas and parent collections
 edu_levels_list = []
 subject_areas_list = []
 parent_collections_list = []
 
-# iterate over the resources dataframe to get the education levels, subject areas and parent collections
-for index, row in resources_df.iterrows():
+# Iterate over the resourcefiles dataframe
+for index, row in resourcefiles_df.iterrows():
     resourceid = row['resourceid']
     edu_level_query = ("SELECT el.`resourceid`, e.`levelid`, e.`displayname` FROM `resource_educationlevels` AS el LEFT JOIN `educationlevels` AS e ON (el.`educationlevelid` = e.`levelid`) WHERE el.`resourceid` = " + str(resourceid) + ";")
     edu_level_df = pd.read_sql(edu_level_query, cnx)
@@ -55,15 +66,65 @@ edu_levels_df = pd.concat(edu_levels_list)
 subject_areas_df = pd.concat(subject_areas_list)
 parent_collections_df = pd.concat(parent_collections_list)
 
-# merge the dataframes with the resources dataframe
-resources_df = pd.merge(resources_df, edu_levels_df, on='resourceid', how='left')
-resources_df = pd.merge(resources_df, subject_areas_df, on='resourceid', how='left')
-resources_df = pd.merge(resources_df, parent_collections_df, on='resourceid', how='left')
+# merge the dataframes with the resourcefiles dataframe
+resourcefiles_df = pd.merge(resourcefiles_df, edu_levels_df, on='resourceid', how='left')
+resourcefiles_df = pd.merge(resourcefiles_df, subject_areas_df, on='resourceid', how='left')
+resourcefiles_df = pd.merge(resourcefiles_df, parent_collections_df, on='resourceid', how='left')
 
+os.environ["AWS_PROFILE"] = 'currikiai'
+s3 = boto3.client('s3')
 
-print(resources_df.head())
+# iterate over the resourcefiles dataframe to extract the text from the pdf files
+for index, row in resourcefiles_df.iterrows():
+    file_name = row['filename']
+    s3_path = row['s3path']
+    # get components of the s3 path which is in the format: "https://currikicdn.s3-us-west-2.amazonaws.com/resourcedocs/54d21ccf0e69b.pdf". here first 'currikicdn' is bucket name. Path after "https://currikicdn.s3-us-west-2.amazonaws.com" is the path in the bucket for the file to be downloaded.
+    s3_bucket = s3_path.split('/')[2].split('.')[0]
+    resourcefile_s3_download_path = '/'.join(s3_path.split('/')[3:])
+    resourcefile_s3_name = resourcefile_s3_download_path.split('/')[-1]
+
+    # download resourcefile_s3_download_path from s3_bucket
+    s3.download_file(s3_bucket, resourcefile_s3_download_path, resourcefile_s3_name)
+
+    fileLoader = None
+    # load the pdf file if row['ext'] is 'pdf'
+    if row['ext'] == 'pdf':
+        fileLoader = PyPDFLoader(resourcefile_s3_name)
+    
+    if fileLoader is not None:
+        loaded_document = fileLoader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(loaded_document)
+
+        content_soup = BeautifulSoup(row['content'], 'html.parser')
+        content_text = content_soup.get_text()
+        # remove newlines and extra spaces
+        content_text = ' '.join(content_text.split())
+
+        # description is in html format, so convert it to text
+        description_soup = BeautifulSoup(row['description'], 'html.parser')
+        description_text = description_soup.get_text()
+        # remove newlines and extra spaces
+        description_text = ' '.join(description_text.split())
+
+        # join row['pageurl'] with base url https://www.currikilibrary.org/
+        pageurl = 'https://www.currikilibrary.org/' + row['pageurl']
+
+        # iterate over the documents and add metadata to each document all columns of the resourcefiles_df
+        for i, doc in enumerate(docs):
+            doc.metadata['title'] = row['title']
+            doc.metadata['pageurl'] = pageurl
+            doc.metadata['description'] = description_text
+            doc.metadata['additionalcontent'] = content_text
+            doc.metadata['keywords'] = row['keywords']
+            doc.metadata['educationlevels'] = row['educationlevels']
+            doc.metadata['subjectareas'] = row['subjectareas']
+            doc.metadata['parentcollections'] = row['parentcollections']
+            
+
+        print(docs)
+        print(f"*** {len(docs)} documents created for {resourcefile_s3_name} ***")
+       
 
 cursor.close()
 cnx.close()
-
-
